42d263432fa87d2fd3a2b181304088f4d6e7705e
shwetabalagumpi5-eng <shwetabalagumpi5@gmail.com>
2026-08-26 15:47:04 +0530

    Add files via upload

--- tic tac toe.cpp
+++ tic tac toe.cpp
@@ -1,82 +1,80 @@
 #include <iostream>
 using namespace std;
 
+void display(char board[3][3]) {
+    cout << "\n";
+    for (int i = 0; i < 3; i++) {
+        cout << " " << board[i][0] << " | " << board[i][1]
+             << " | " << board[i][2] << "\n";
+        if (i < 2)
+            cout << "---|---|---\n";
+    }
+    cout << "\n";
+}
+
+bool win(char board[3][3], char p) {
+    // Rows and columns
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == p && board[i][1] == p && board[i][2] == p)
+            return true;
+
+        if (board[0][i] == p && board[1][i] == p && board[2][i] == p)
+            return true;
+    }
+
+    // Diagonals
+    if (board[0][0] == p && board[1][1] == p && board[2][2] == p)
+        return true;
+
+    if (board[0][2] == p && board[1][1] == p && board[2][0] == p)
+        return true;
+
+    return false;
+}
+
 int main() {
     char board[3][3] = {
-        {'1', '2', '3'},
-        {'4', '5', '6'},
-        {'7', '8', '9'}
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}
     };
 
     char player = 'X';
-    int choice, row, col;
+    int row, col;
 
     for (int turn = 0; turn < 9; turn++) {
+        display(board);
 
-        // Display board
-        cout << "\n";
-        for (int i = 0; i < 3; i++) {
-            cout << " " << board[i][0] << " | "
-                 << board[i][1] << " | "
-                 << board[i][2] << "\n";
-            if (i < 2)
-                cout << "---|---|---\n";
-        }
+        cout << "Player " << player << ", enter row and column (1-3): ";
+        cin >> row >> col;
 
-        cout << "\nPlayer " << player << ", enter position (1-9): ";
-        cin >> choice;
-
-        // Convert position to row and column
-        row = (choice - 1) / 3;
-        col = (choice - 1) % 3;
-
-        // Check valid move
-        if (choice < 1 || choice > 9 ||
-            board[row][col] == 'X' || board[row][col] == 'O') {
+        // Validate move
+        if (row < 1 || row > 3 || col < 1 || col > 3 ||
+            board[row - 1][col - 1] != ' ') {
             cout << "Invalid move! Try again.\n";
             turn--;
             continue;
         }
 
-        board[row][col] = player;
+        board[row - 1][col - 1] = player;
 
-        // Check rows and columns
-        bool win = false;
-
-        for (int i = 0; i < 3; i++) {
-            if (board[i][0] == player &&
-                board[i][1] == player &&
-                board[i][2] == player)
-                win = true;
-
-            if (board[0][i] == player &&
-                board[1][i] == player &&
-                board[2][i] == player)
-                win = true;
-        }
-
-        // Check diagonals
-        if (board[0][0] == player &&
-            board[1][1] == player &&
-            board[2][2] == player)
-            win = true;
-
-        if (board[0][2] == player &&
-            board[1][1] == player &&
-            board[2][0] == player)
-            win = true;
-
-        if (win) {
-            cout << "\nPlayer " << player << " wins! ??\n";
-            break;
+        // Check winner
+        if (win(board, player)) {
+            display(board);
+            cout << "Player " << player << " wins!\n";
+            return 0;
         }
 
         // Switch player
-        player = (player == 'X') ? 'O' : 'X';
-
-        if (turn == 8)
-            cout << "\nGame Draw!\n";
+        if (player == 'X')
+            player = 'O';
+        else
+            player = 'X';
     }
+
+    display(board);
+    cout << "It's a draw!\n";
 
     return 0;
 }
+
